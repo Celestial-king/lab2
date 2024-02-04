f1b20a5334cc690bb17b591dd688192d6c4f8a96
Celestial-King <jedn2004@gmail.com>
2024-02-04 22:37:47 +0800

    editedweek8

---
+++ week8/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Jose's Portfolio</title>
+    <link rel="stylesheet" href="style index.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="icon" type="image/x-icon" href="icon.png">
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="logo">Jose Enrique D. Nunez</a>
+        <i class='bx bx-menu' id="menu-icon"></i>
+        <nav class="navbar">
+          <a href="index.html">Home</a>
+          <a href="about.html">About</a>
+          <a href="#">Portfolio</a>
+          <a href="contact.html">Services</a>
+          <a href="contact.html">Contact</a>
+        </nav>
+    </header>
+    <div class="nav-bg"></div>
+    
+    <section class="home">
+      <div class="homeContent">
+          <h3>Hello, I am</h3>
+          <h1>Jose Enrique D.Nunez</h1>
+          <h2>A.K.A Celestial <span class="typing_text2"></span></h2>
+          <p> I am passionate front-end developer specializing in translating design concepts into seamless and interactive web experiences using HTML, CSS, and JavaScript.</p>
+          <div class="socialMedia">
+              <a href="https://github.com/Celestial-king" target="_blank"><i class='bx bxl-github'></i></a>
+              <a href="https://www.linkedin.com/in/jose-enrique-nuñez-celestial/" target="_blank"><i class='bx bxl-linkedin' ></i></a>
+              <a href="https://www.facebook.com/profile.php?id=100079851079655" target="_blank"><i class='bx bxl-facebook' ></i></a>
+              <a href="https://www.instagram.com/nunezzenrique/" target="_blank"><i class='bx bxl-instagram' ></i></a>
+          </div>
+          <div class="btn">
+          <button> <a href="https://www.linkedin.com/in/jose-enrique-nuñez-celestial/" target="_blank">Connect with me</a></button>
+          </div>
+        </div>
+
+      <div class="right">
+        <div class="profile">
+          <img src="CELESTIAL.jpg" alt="profile">
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
